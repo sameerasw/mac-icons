92a900a887c2a6014d8e959937130c6dca9b4dad
sameerasw <sameera.whoami@gmail.com>
2024-03-03 03:14:36 +0530

    fixed preview webpage

--- preview/script.js
+++ preview/script.js
@@ -1,40 +1,35 @@
 document.addEventListener("DOMContentLoaded", async function () {
-    // Specify the subdirectory paths
-    var foldericons = 'folder-icons/';
-    var appicons = 'app-icons/';
+    // Specify the GitHub repository information
+    var username = 'sameerasw';
+    var repo = 'mac-icons';
+    var path1 = 'folder-icons/';
+    var path2 = 'app-icons/';
 
     // Get the imageGallery container
     var imageGalleryContainer = document.getElementById('grid');
 
+    function fetchImages(path) {
+        return fetch(`https://api.github.com/repos/${username}/${repo}/contents/${path}`)
+            .then(response => response.json())
+            .then(data => {
+                // Filter only the image files
+                var imageFiles = data.filter(item => item.type === 'file' && item.name.match(/\.(jpg|jpeg|png|gif)$/i));
+
+                // Generate img tags and append them to the imageGallery container
+                imageFiles.forEach(function (imageFile) {
+                    var img = document.createElement('img');
+                    img.src = imageFile.download_url;
+                    img.alt = imageFile.name; // You can set alt text if needed
+                    imageGalleryContainer.appendChild(img);
+                });
+            })
+            .catch(error => console.error('Error fetching images:', error));
+    }
+
     try {
         // Fetch the list of images in the subdirectories
-        const [folderIconsResponse, appIconsResponse] = await Promise.all([
-            fetch(foldericons),
-            fetch(appicons)
-        ]);
-
-        // Parse the HTML responses
-        const [folderIconsHtml, appIconsHtml] = await Promise.all([
-            folderIconsResponse.text(),
-            appIconsResponse.text()
-        ]);
-
-        // Create a temporary div to parse the HTML
-        var tempDiv = document.createElement('div');
-        tempDiv.innerHTML = folderIconsHtml + appIconsHtml;
-
-        // Get all the image file names if it's a .png file
-        var imageFiles = Array.from(tempDiv.querySelectorAll('a'))
-            .map(a => a.href)
-            .filter(href => href.match(/\.(png)$/));
-
-        // Generate img tags and append them to the imageGallery container
-        imageFiles.forEach(function (imageFile) {
-            var img = document.createElement('img');
-            img.src = imageFile;
-            img.alt = imageFile; // You can set alt text if needed
-            imageGalleryContainer.appendChild(img);
-        });
+        Promise.all([fetchImages(path1), fetchImages(path2)])
+            .catch(error => console.error('Error fetching images:', error));
     } catch (error) {
         console.error('Error fetching images:', error);
     }
